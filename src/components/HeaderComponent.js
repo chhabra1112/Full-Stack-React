b8b766516789a7b66e163688a3278695f9acbcf5
Piyush Chhabra <piyushchhabra.1198@gmail.com>
2020-06-28 20:53:17 +0530

    redux-form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,68 +1,115 @@
 import React, { Component } from 'react';
-import { Navbar,NavbarBrand,Nav,NavbarToggler,Collapse,NavItem, Jumbotron } from 'reactstrap';
-import {NavLink} from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron, Modal, ModalHeader, ModalBody, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 
 class Header extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            isNavOpen: false
+        this.state = {
+            isNavOpen: false,
+            isModalOpen: false
         }
         this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
-    toggleNav(){
+    toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
+
         });
+    }
+    toggleModal() {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Username: " + this.username.value + " Password: " + this.password.value + " Remember: " + this.remember.checked);
+        event.preventDefault();
     }
     render() {
         return (
             <>
-                <Navbar dark expand = "md" >
-                <div className="container">
-                    <NavbarToggler onClick={this.toggleNav}/>
-                <NavbarBrand className="mr-auto" href="/">
-                    <img src="Assets/images/logo.png" height="30" width="41" alt="Restorante Con Fusion"/>
-                </NavbarBrand>
-                <Collapse isOpen={this.state.isNavOpen} navbar>
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/home">
-                            <span className="fa fa-home fa-lg"/> Home
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/aboutus">
-                            <span className="fa fa-info fa-lg"/> About Us
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/menu">
-                            <span className="fa fa-list fa-lg"/> Menu
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className="nav-link" to="/contactus">
-                            <span className="fa fa-address-card fa-lg"/>Contact Us
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-                </Collapse>
+                <Navbar dark expand="md" >
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="Assets/images/logo.png" height="30" width="41" alt="Restorante Con Fusion" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/home">
+                                        <span className="fa fa-home fa-lg" /> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg" /> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg" /> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg" /> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <Button outline onClick={this.toggleModal}>
+                                        <span className="fa fa-sign-in fa-lg" /> Login
+                                    </Button>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron>
-                <div className="container">
-                    <div className="row row-header">
-                    <div className="col-12 col-sm-6">
-                    <h1>Ristorante con Fusion</h1>
-                       <p>We take inspiration from the World's best cuisines, 
-                           and create a unique fusion experience. Our lipsmacking 
+                    <div className="container">
+                        <div className="row row-header">
+                            <div className="col-12 col-sm-6">
+                                <h1>Ristorante con Fusion</h1>
+                                <p>We take inspiration from the World's best cuisines,
+                                and create a unique fusion experience. Our lipsmacking
                            creations will tickle your culinary senses!</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                </div>
                 </Jumbotron>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input}></Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}></Input>
+                            </FormGroup>
+                            <FormGroup className="ml-3">
+                                <Label check>
+                                    <Input type="checkbox" id="remember" name="remember"
+                                        innerRef={(input) => this.remember = input} />Remember Me
+                                </Label>
+                            </FormGroup>
+                            <FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </>
         )
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent'
 import Menu from './MenuComponent';
-import {DISHES} from '../shared/dishes';
-import {COMMENTS} from '../shared/comments';
-import {LEADERS} from '../shared/leaders';
-import {PROMOTIONS} from '../shared/promotions';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {Switch, Route,Redirect} from 'react-router-dom';
+import {Switch, Route,Redirect,withRouter} from 'react-router-dom';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
+import {connect} from 'react-redux';
+
+const mapStatetoProps = state => {
+  return {
+    dishes : state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 
 class Main extends Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-    dishes: DISHES,
-    comments:COMMENTS,
-    promotions:PROMOTIONS,
-    leaders:LEADERS
-    };
-  }
   
 
   render(){
     const HomePage = ()=>{
       return(
-        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-        promotion={this.state.promotions.filter((promotion)=>promotion.featured)[0]}
-        leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+        <Home dish={this.props.dishes.filter((dish)=>dish.featured)[0]}
+        promotion={this.props.promotions.filter((promotion)=>promotion.featured)[0]}
+        leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
         />
       );
     };
     const  dishWithId = ({match})=>{
       return(
-        <DishDetail dish = {this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
-        comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}/>
+        <DishDetail dish = {this.props.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishId,10))}/>
       );
     };
 
@@ -47,10 +43,10 @@ class Main extends Component{
       <div className="container">
       <Switch>
         <Route path="/home" component= {HomePage}/>
-        <Route exact path="/menu" component = {()=> <Menu dishes={this.state.dishes}/>}/>
+        <Route exact path="/menu" component = {()=> <Menu dishes={this.props.dishes}/>}/>
         <Route path="/menu/:dishId" component={dishWithId}/>
         <Route exact path='/contactus' component={Contact} />
-        <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
+        <Route exact path='/aboutus' component={()=><About leaders={this.props.leaders}/>} />
         <Redirect to ="/home"/>
       </Switch>
       </div>
@@ -60,4 +56,4 @@ class Main extends Component{
 }
 }
 
-export default Main;
+export default withRouter(connect(mapStatetoProps)(Main));
